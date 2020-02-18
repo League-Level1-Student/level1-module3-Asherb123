cffa931deb838f4a06134f97e70282273806bcbe
League Student <league@iMac.attlocal.net>
2020-02-17 18:27:22 -0800

    yooooyoyoyoyoyooyoy

--- src/_06_gridworld/GridWorldRunner.java
+++ src/_06_gridworld/GridWorldRunner.java
@@ -1,5 +1,10 @@
 package _06_gridworld;
 
+import java.awt.Color;
+
+import info.gridworld.actor.Bug;
+import info.gridworld.actor.Flower;
+import info.gridworld.grid.Location;
 import info.gridworld.world.World;
 
 public class GridWorldRunner {
@@ -8,17 +13,22 @@ public static void main(String[] args) {
 World w = new World();
 
 w.show();
+Bug b=new Bug();
+w.add(new Location(5,6),b);
+
+b.turn();
 
 
 
 
 
+Flower f = new Flower();
 
+w.add (new Location (4,6), f);
 
+w.add (new Location (6,6), f);
 
-
-
-
+w.add (new Location (6,6), f);
 
 
 
--- src/_07_cow_timer/CowTimerRunner.java
+++ src/_07_cow_timer/CowTimerRunner.java
@@ -5,5 +5,38 @@ public class CowTimerRunner {
 		/* Make a CowTimer, set its time and start it.
 		 * Use a short delay (seconds) when testing, then try with longer delays */
 
+		
+		CowTimer t = new CowTimer();
+		
+		
+	for (int i = 10; i > 0; i--) {
+		Thread.sleep(1000);
+	
+	System.out.println(i);
+	}
+		
+		
+		
+	
+	
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 }
